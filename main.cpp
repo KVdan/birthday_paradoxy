6ba4cbf84bda4b6d21f333f13b55a207136cffc6
KVdan <dainewu2017@gmail.com>
2020-09-16 23:15:57 +0800

    add main.cpp file

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    int p=0;
+    float res = 1;
+    float num=365;
+    float n=365;
+
+    while(res>0.5)
+    {
+        res *=(n/num);
+        n--;
+        p++;
+    }
+    std::cout<<"When # of people equals "<<p<<", the probability that there exists two people born at the same day is no less than 0.5."<<std::endl;
+
+    return 0;
+}
